007e285a8efeeb075e21807a8e119b961211e02a
chinnysandra1 <169098646+chinnysandra1@users.noreply.github.com>
2024-08-13 21:59:14 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let guests= ["favour","steph","chinelo"]
+let visitorsname =prompt("what is your name?")
+if(guests.includes(visitorsname)){
+    alert("welcome to my party click below to gain access");
+
+}else{
+    alert(" Dear "+visitorsname+", don't embarass yourself and getout of here, at your age you still go to where you're not invited, shame on you");
+
+}
